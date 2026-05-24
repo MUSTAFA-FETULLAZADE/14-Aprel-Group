89b1796b7de397e98d668ccbb484c9e54e098f3f
MUSTAFA-FETULLAZADE <mustafafetullazade2025@gmail.com>
2026-05-24 12:28:26 +0400

    HOMECONTROLLER

--- Spring/spring-project-14aprel/spring-project-14aprel/src/main/java/az/developia/spring_project_14aprel/controller/HomeController.java
+++ Spring/spring-project-14aprel/spring-project-14aprel/src/main/java/az/developia/spring_project_14aprel/controller/HomeController.java
@@ -1,14 +1,22 @@
 package az.developia.spring_project_14aprel.controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String getHomePage() {
-        return "home"; 
+   
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+     
+        logger.info("Hello endpoint-i çağırıldı!");
+        
+        return "Salam! Log faylına baxmağı unutmayın.";
     }
 }
